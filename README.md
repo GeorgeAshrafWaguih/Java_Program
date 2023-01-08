a6ba9ca2ce81d31ed144a4b892cf5e819b86498c
George <George.ashraf@hotmail.com>
2023-01-08 21:23:51 +0200

    Nested IFs

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Java_Program
 Java code covering the following: 
 
-    1- Java fundamentals
+    1- Java fundamentals (GrossPayCalculator)
     2- Decision structure:
-        a- If 
-        b- If Else
-        c- Nested If
+        a- If (SalaryCalculator)
+        b- If Else (QuotaCalculator)
+        c- Nested If (LoanQualifier)
         d- If Else If 
         e- Switch 
         f- Relational and Logical operators
---
+++ src/decisionStructure/LoanQualifier.java
@@ -0,0 +1,37 @@
+package decisionStructure;
+
+import java.util.Scanner;
+
+/*
+Nested IFs
+To qualify for a loan , a person must make at least $30,000
+and have been working at their current job for at least 2 years
+ */
+public class LoanQualifier {
+    public static void main(String args[]){
+
+        // Initialize what we know
+        int requiredSalary = 30000;
+        int requiredYearsEmployed = 2;
+
+        // Get what we don't know
+        System.out.println("Enter your Salary: ");
+        Scanner scanner = new Scanner(System.in);
+        double salary = scanner.nextDouble();
+
+        System.out.println("Enter the number of years at your current job: ");
+        double years = scanner.nextDouble();
+        scanner.close();
+
+        // Make decision
+        if(salary >= requiredSalary){
+            if (years >= requiredYearsEmployed){
+                System.out.println("Congrats! You qualify for the loan");
+            }else {
+                System.out.println("Sorry you must work for your current job for "+requiredYearsEmployed+" years");
+            }
+        }else {
+            System.out.println("Sorry your salary needs to be at least $"+requiredSalary);
+        }
+    }
+}
